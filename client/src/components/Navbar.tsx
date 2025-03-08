01543121a7082aea637417b04c26e157dcdca27e
Nasir-Buddy <syedali172000@gmail.com>
2025-03-09 03:33:02 +0530

    change button color

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -45,8 +45,8 @@ const Navbar = () => {
 
                     <Link 
                     href="/signin">
-                        <Button variant="outline"
-                        className='text-white border-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg'
+                        <Button variant="secondary"
+                        className='text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg'
                         >
                             Sign Up
                         </Button>
