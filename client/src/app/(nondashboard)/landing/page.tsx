d2cff59d5bc69d296fe2ddc310a72de5d172a364
Nasir-Buddy <syedali172000@gmail.com>
2025-03-16 18:41:39 +0530

    completed the discover section

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from './HeroSection'
 import FeatureSection from './FeatureSection'
+import DiscoverSection from './DiscoverSection'
 const page = () => {
   return (
     <div>
       <HeroSection />
       <FeatureSection />
+      <DiscoverSection />
     </div>
   )
 }
