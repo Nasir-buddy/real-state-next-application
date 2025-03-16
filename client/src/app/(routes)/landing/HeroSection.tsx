a4ec81ff363222171cf0535600ad241fa1833145
Nasir-Buddy <syedali172000@gmail.com>
2025-03-16 17:08:37 +0530

    fix issue

--- client/src/app/(routes)/landing/HeroSection.tsx
+++ client/src/app/(routes)/landing/HeroSection.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className="relative h-screen">
+      <Image
+        src="/landing-splash.jpg"
+        alt="Rentiful Rental Platform Hero Section"
+        fill
+        className="object-cover object-center "
+        priority
+      />
+      <div className="absolute inset-0 bg-black opacity-60 flex items-center justify-center">
+      
+      </div>
+    </div>
   )
 }
 
