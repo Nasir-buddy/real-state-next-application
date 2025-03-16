8e864c29582d8c6151a60adeb6b9d57e6a9f9f48
Nasir-Buddy <syedali172000@gmail.com>
2025-03-16 23:50:46 +0530

    setup call to action

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import FeatureSection from './FeatureSection'
 import DiscoverSection from './DiscoverSection'
+import CalltoActionSection from './CalltoActionSection'
 const page = () => {
   return (
     <div>
       <HeroSection />
       <FeatureSection />
       <DiscoverSection />
+      <CalltoActionSection />
     </div>
   )
 }
