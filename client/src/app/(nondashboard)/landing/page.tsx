9c63fa27cbef894902f1209d6c7475ba1aedc4be
Nasir-Buddy <syedali172000@gmail.com>
2025-03-16 17:56:42 +0530

    created feature section

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import HeroSection from './HeroSection'
-
+import FeatureSection from './FeatureSection'
 const page = () => {
   return (
     <div>
       <HeroSection />
+      <FeatureSection />
     </div>
   )
 }
