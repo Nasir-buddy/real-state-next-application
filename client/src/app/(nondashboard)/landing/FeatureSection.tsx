4a8d60b7e79e32a3bcb884ad2f91960863e1a2fd
Nasir-Buddy <syedali172000@gmail.com>
2025-03-16 17:59:00 +0530

    set constant variables

--- client/src/app/(nondashboard)/landing/FeatureSection.tsx
+++ client/src/app/(nondashboard)/landing/FeatureSection.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
 const FeatureSection = () => {
+    const containerVariants = { 
+        hidden: { opacity: 0, y: 50 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.5,
+                staggerChildren: 0.2
+            } 
+        }
+    }
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 },
+    }
+
+    
   return (
     <div>FeatureSection</div>
   )
