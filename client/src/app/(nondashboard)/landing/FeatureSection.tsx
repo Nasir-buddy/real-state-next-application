5f7c8f69b1d2c95693cdbc69e14d43d84e51ff76
Nasir-Buddy <syedali172000@gmail.com>
2025-03-16 18:05:56 +0530

    setting up the card initial code

--- client/src/app/(nondashboard)/landing/FeatureSection.tsx
+++ client/src/app/(nondashboard)/landing/FeatureSection.tsx
@@ -1,7 +1,8 @@
+'use client'
 import React from 'react'
 import { motion } from 'framer-motion'
 const FeatureSection = () => {
-    const containerVariants = { 
+    const containerVariants = {
         hidden: { opacity: 0, y: 50 },
         visible: {
             opacity: 1,
@@ -9,7 +10,7 @@ const FeatureSection = () => {
             transition: {
                 duration: 0.5,
                 staggerChildren: 0.2
-            } 
+            }
         }
     }
 
@@ -18,10 +19,32 @@ const FeatureSection = () => {
         visible: { opacity: 1, y: 0 },
     }
 
-    
-  return (
-    <div>FeatureSection</div>
-  )
-}
+
+    return (
+        <motion.div
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true }}
+            variants={containerVariants}
+            className='py-24 px-6 sm:px-8 lg:px-12 xl:px16 bg-white'
+        >
+            <div className='max-w-4xl xl:max-7xl mx:auto'>
+                <motion.h2 
+                variants={itemVariants}
+                className='text-3xl font-bold text-center mb-12 w-full sm:w-2/3 mx-auto'
+                >
+                    Quickly find the home you want using our effective search filters!
+                </motion.h2>
+                <div className='grid grid-col-1 md:grid-col-3 gap-8 lg:gap-12 xl:gap-16'>
+                    {[0,1,2].map((index) => (
+                        <motion.div key={index} variants={itemVariants}>
+
+                        </motion.div>
+                    ))}
+                </div>
+            </div>
+        </motion.div>
+    );
+};
 
 export default FeatureSection
