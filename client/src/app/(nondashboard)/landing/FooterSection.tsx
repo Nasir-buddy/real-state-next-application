9442597bca36720ce307001c09534184aa670e5b
Nasir-Buddy <syedali172000@gmail.com>
2025-03-17 00:51:36 +0530

    completed footer

--- client/src/app/(nondashboard)/landing/FooterSection.tsx
+++ client/src/app/(nondashboard)/landing/FooterSection.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
-
+import Link from 'next/link';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faFacebook,
+    faInstagram,
+    faTwitter,
+    faLinkedin,
+    faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
 const FooterSection = () => {
-  return (
-    <div>FooterSection</div>
-  )
-}
+    return (
+        <footer className='border-t border-gray-200 py-20'>
+            <div className='max-w-4xl mx-auto px-6 sm:px-8'>
+                <div className='flex flex-col justify-between items-center'>
+                    <div className='mb-4'>
+                        <Link
+                            href='/'
+                            className='text-xl font-bold'
+                            scroll={false}
+                        >
+                            RENTIFUL
+                        </Link>
+                    </div>
+                    <nav className='mb-4'>
+                        <ul className='flex space-x-6'>
+                            <li>
+                                <Link href='/about'>About Us</Link>
+                            </li>
+                            <li>
+                                <Link href='/contact'>Contact</Link>
+                            </li>
+                            <li>
+                                <Link href='/terms'>Terms</Link>
+                            </li>
+                            <li>
+                                <Link href='/privacy'>Privacy</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className='flex space-x-4 mb-4'>
+                        <a
+                            title='facebook'
+                            href="#"
+                            area-label="Facebook"
+                            className='hover:text-primary-600'
+                        ><FontAwesomeIcon icon={faFacebook} className='h-6 w-6' />
+                        </a>
+                        <a
+                            title='instagram'
+                            href="#"
+                            area-label="Instagram"
+                            className='hover:text-primary-600'
+                        ><FontAwesomeIcon icon={faInstagram} className='h-6 w-6' />
+                        </a>
+                        <a
+                            title='Linkedin'
+                            href="#"
+                            area-label="Linkedin"
+                            className='hover:text-primary-600'
+                        ><FontAwesomeIcon icon={faLinkedin} className='h-6 w-6' />
+                        </a>
+                        <a
+                            title='youtube'
+                            href="#"
+                            area-label="Youtube"
+                            className='hover:text-primary-600'
+                        ><FontAwesomeIcon icon={faYoutube} className='h-6 w-6' />
+                        </a>
+                    </div>
+                </div>
+                <div className='mt-8 text-center text-sm text-gray-500 flex justify-center space-x-4'>
+                    <span>© RENTiful. All rights reserved.</span>
+                    <Link href='/privacy'>Privacy Policy</Link>
+                    <Link href='/terms'>Terms of Services</Link>
+                    <Link href='/cookies'>Cookie Policy</Link>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default FooterSection
+export default FooterSection;
