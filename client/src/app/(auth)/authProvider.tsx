a58e4cc88baf333ba2e7d2adcb5347d55e9a5534
Nasir-Buddy <syedali172000@gmail.com>
2025-03-18 22:22:55 +0530

    form schema setup

--- client/src/app/(auth)/authProvider.tsx
+++ client/src/app/(auth)/authProvider.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 
-import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator, components, useAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { FormField } from '@/components/ui/form';
 
 // import awsExports from './aws-exports';
 Amplify.configure({
@@ -14,11 +15,56 @@ Amplify.configure({
     }
 });
 
+const formFields = {
+    signIn: {
+        username: {
+            placeholder: "Enter you email",
+            label: "Email",
+            isRequired: true
+        }, 
+        password: {
+            placeholder: "Enter you password",
+            label: "password",
+            isRequired: true
+        }
+    }, 
+    signUp: {
+        username: 
+        {   
+            order: 1,
+            placeholder: "Choose a username",
+            label: "username",
+            isRequired: true
+        }, 
+        email: {
+            order: 2,
+            placeholder: "Enter you eamil address",
+            label: "Email",
+            isRequired: true
+        },
+        password: {
+            order: 3,
+            placeholder: "Create a password",
+            label: "password",
+            isRequired: true
+        }, 
+        confirm_password: {
+            order: 3,
+            placeholder: "Confirm your password",
+            label: "Confirm Password",
+            isRequired: true
+        }
+    }
+}
+
 const Auth = ({ children }: { children: React.ReactNode }) => {
     const { user } = useAuthenticator((context) => [context.user])
     return (
         <div className='h-full'>
-            <Authenticator>
+            <Authenticator
+                components={components}
+                formFields={FormField}
+            >
                 {() => <>{children}</>}
             </Authenticator>
         </div>
