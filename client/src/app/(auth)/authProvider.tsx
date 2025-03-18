517c66903af61de21062cd31451e7b6bfe179d3f
Nasir-Buddy <syedali172000@gmail.com>
2025-03-18 22:39:11 +0530

    created signup footer

--- client/src/app/(auth)/authProvider.tsx
+++ client/src/app/(auth)/authProvider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 
-import { Authenticator, components, useAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator, components, Heading, useAuthenticator, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { FormField } from '@/components/ui/form';
 
@@ -15,27 +15,60 @@ Amplify.configure({
     }
 });
 
+const components = {
+    Header() {
+        return (
+            <View className='mt-4 mb-7'>
+                <Heading level={3} className='!text-2xl @font-bold'>
+                    RENT
+                    <span className='text-secondary-500 font-light hover:!text-primary-300'>IFUL</span>
+                </Heading>
+                <p className='text-muted-foreground mt-2'>
+                    <span className='font-bold '>Welcome!</span> Please sign in to continue
+                </p>
+            </View>
+        )
+    },
+    SignIn: {
+        Footer() {
+            const { toSignUp } = useAuthenticator();
+            return (
+                <View className='text-center mt-4'>
+                    <p className='text-muted-foreground '>
+                        Don&apos;t have an account?{" "}
+                        <button
+                            onClick={toSignUp}  
+                            className='text-primary hover:underline bg-transparent border-none p-0'
+                        >
+                            Sign up here
+                        </button>
+                    </p>
+                </View>
+            )
+        }
+    }
+}
 const formFields = {
     signIn: {
         username: {
             placeholder: "Enter you email",
             label: "Email",
             isRequired: true
-        }, 
+        },
         password: {
             placeholder: "Enter you password",
             label: "password",
             isRequired: true
         }
-    }, 
+    },
     signUp: {
-        username: 
-        {   
+        username:
+        {
             order: 1,
             placeholder: "Choose a username",
             label: "username",
             isRequired: true
-        }, 
+        },
         email: {
             order: 2,
             placeholder: "Enter you eamil address",
@@ -47,7 +80,7 @@ const formFields = {
             placeholder: "Create a password",
             label: "password",
             isRequired: true
-        }, 
+        },
         confirm_password: {
             order: 3,
             placeholder: "Confirm your password",
