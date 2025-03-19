ccaa3bffbf2828276fd5fd803150e5bac3d49d6f
Nasir-Buddy <syedali172000@gmail.com>
2025-03-19 23:48:52 +0530

    feat: add more items to the navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,8 +3,22 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
+import { useGetAuthUserQuery } from '@/state/api'
+import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
+import { signOut } from 'aws-amplify/auth'
+import { MessageCircle, Plus, Search } from 'lucide-react'
 
 const Navbar = () => {
+    const { data: authUser } = useGetAuthUserQuery();
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const isDashboardPage = pathname.includes('/managers') || pathname.includes('tenants');
+    const handleSignOut = async () => {
+        await signOut();
+        window.location.href = "/";
+    }
     return (
         <div className='fixed h-fit bg-amber-200 top-0 left-0 w-full z-50 shadow-xl'>
             <div className='flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white'>
@@ -25,32 +39,77 @@ const Navbar = () => {
                                 <span className='text-secondary-500 font-light hover:!text-primary-300'>
                                     IFUL
                                 </span>
+                                {isDashboardPage && authUser && (
+                                    <Button
+                                        variant='secondary'
+                                        className='md:ml-4 bg-primary-50 text-primary-700 hover:bg-secondary-500 hover:text-primary-50'
+                                        onClick={() => {
+                                            router.push(
+                                                authUser.userRole.toLowerCase() === 'manager' ? '/manager/newproperty' : '/search'
+                                            )
+                                        }}
+                                    >
+                                        {authUser.userRole.toLowerCase() === 'manager' ? (<>
+                                            <Plus
+                                                className='h-4 w-4'
+                                            >
+                                                <span className='hidden md:block ml-2'>Add New Property</span>
+                                            </Plus>
+                                        </>) : (
+                                            <>
+                                                <Search className='h-4 w-4' />
+                                                <span className='hidden md:block ml-2'>
+                                                    Search Properties
+                                                </span>
+                                            </>
+                                        )}
+                                    </Button>
+                                )}
                             </div>
                         </div>
                     </Link>
 
                 </div>
-                <p className='text-primary-200 hidden md:block'>
+                {!isDashboardPage && (<p className='text-primary-200 hidden md:block'>
                     Discover your perfect rental apartment with our advance search
-                </p>
+                </p>)}
                 <div className='flex items-center gap-5'>
-                    <Link 
-                    href="/signin">
-                        <Button variant="secondary"
-                        className='text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-xl'
-                        >
-                            Sign In
-                        </Button>
-                    </Link>
 
-                    <Link 
-                    href="/signup">
-                        <Button variant="secondary"
-                        className='text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg'
-                        >
-                            Sign Up
-                        </Button>
-                    </Link>
+                    {
+                        authUser ?
+                            (
+                                <>
+                                    <div className='relative hidden md:block'>
+                                        <MessageCircle className='w-6 h-6 cursor-pointer text-primary-200 hover:text-primary-500' />
+                                        <span className='absolute top-0 right-0 w-2 h-2 bg-secondary-700 rounded-full'></span>
+                                    </div>
+                                    <div>
+
+                                    </div>
+                                </>
+                            )
+                            :
+                            (
+                                <>
+                                    <Link
+                                        href="/signin">
+                                        <Button variant="secondary"
+                                            className='text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-xl'
+                                        >
+                                            Sign In
+                                        </Button>
+                                    </Link>
+                                    <Link
+                                        href="/signup">
+                                        <Button variant="secondary"
+                                            className='text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg'
+                                        >
+                                            Sign Up
+                                        </Button>
+                                    </Link>
+                                </>
+                            )
+                    }
                 </div>
             </div>
         </div>
