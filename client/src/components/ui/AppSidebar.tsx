3f7f65cf3d3975c9eee1afda70905059e5c96618
Nasir-Buddy <syedali172000@gmail.com>
2025-03-20 00:47:20 +0530

    added side bar content data

--- client/src/components/ui/AppSidebar.tsx
+++ client/src/components/ui/AppSidebar.tsx
@@ -1,13 +1,44 @@
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import { useSidebar } from './sidebar';
+import { Building, FileText, Heart, Settings } from 'lucide-react';
+import { Sidebar } from '@/components/ui/sidebar'
+import Home from '@/app/page';
 
 const AppSidebar = ({ userType }: AppSidebarProps) => {
     const pathname = usePathname();
     const { toggleSidebar, open } = useSidebar();
-    return (
-        <div>AppSidebar</div>
-    )
+
+    const navLinks =
+        userType === 'manager'
+            ? [
+                {
+                    icon: Building, label: "Properties", href: "/managers/properties"
+                },
+                {
+                    icon: FileText, label: "Applications", href: "/managers/applications"
+                },
+                {
+                    icon: Settings, label: "Settings", href: "/managers/settings"
+                }
+            ]
+            : [
+                {
+                    icon: Heart, label: "Favorites", href: "/managers/favorites"
+                },
+                {
+                    icon: FileText, label: "Applications", href: "/managers/applications"
+                },
+                {
+                    icon: Home, label: "Residences", href: "/tenants/settings"
+                },
+                {
+                    icon: Settings, label: "Settings", href: "/managers/settings"
+                }
+            ]
+    return <Sidebar>
+
+    </Sidebar>
 }
 
 export default AppSidebar
