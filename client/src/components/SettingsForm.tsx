1c5b5d9dbb7bf053420d654c24be095cb932992c
Nasir-Buddy <syedali172000@gmail.com>
2025-03-21 03:27:02 +0530

    setup form field and setting form

--- client/src/components/SettingsForm.tsx
+++ client/src/components/SettingsForm.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { SettingsFormData, settingsSchema } from '@/lib/schemas';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { IndianRupeeIcon } from 'lucide-react';
+import { Form } from './ui/form';
+import { CustomFormField } from './FormField';
 
-const SettingsForm = () => {
-  return (
-    <div>SettingsForm</div>
-  )
+const SettingsForm = ({
+    initialData,
+    onSubmit,
+    userType
+}: SettingsFormProps) => {
+    const [editMode, setEditMode] = useState(false);
+    const form = useForm<SettingsFormData>({
+        resolver: zodResolver(settingsSchema),
+        defaultValues: initialData
+    });
+
+    const toggleEditMode = () => {
+        setEditMode(!editMode);
+        if (editMode) {
+            form.reset(initialData);
+        }
+    }
+
+    const handleSubmit = async (data: SettingsFormData) => {
+        await onSubmit(data);
+        setEditMode(false);
+    }
+    return (
+        <div className='pt-8 pb-5 px-8'>
+            <div className='mb-5'>
+                <h1 className='text-xl font-semibold'>{`${userType.charAt(0).toUpperCase() + userType.slice(1)}Settings`}</h1>
+                <p className='text-sm text-gray-500 mt-1'>Manager your preference and personal information</p>
+            </div>
+
+            <div className='bg-white rounded-xl p-6'>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(handleSubmit)}
+                        className='space-y-6'
+                    >
+                        <CustomFormField name='name' label='Name' disabled={!editMode} />
+                    </form>
+                </Form>
+            </div>
+        </div>
+    )
 }
 
 export default SettingsForm
