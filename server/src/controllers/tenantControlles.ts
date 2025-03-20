56c20ff14b499f2ad0b37ed31b52d7345d535f50
Nasir-Buddy <syedali172000@gmail.com>
2025-03-21 02:40:22 +0530

    implemented the update tenant controller

--- server/src/controllers/tenantControlles.ts
+++ server/src/controllers/tenantControlles.ts
@@ -29,9 +29,9 @@ export const createTenant = async (req: Request, res: Response): Promise<void> =
         const { cognitoId, name, email, phoneNumber } = req.body;
         const tenant = await prisma.tenant.create({
             data: {
-                cognitoId, 
+                cognitoId,
                 name,
-                email, 
+                email,
                 phoneNumber,
             }
         });
@@ -41,3 +41,21 @@ export const createTenant = async (req: Request, res: Response): Promise<void> =
     }
 };
 
+
+export const updateTenant = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { cognitoId } = req.params;
+        const { name, email, phoneNumber } = req.body;
+        const updateTenant = await prisma.tenant.update({
+            where: { cognitoId },
+            data: {
+                name,
+                email,
+                phoneNumber,
+            }
+        });
+        res.json(updateTenant);
+    } catch (error) {
+        res.status(500).json({ message: `Error updating tenant: ${error}` })
+    }
+};
--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import {
     getTenant,
-    createTenant
+    createTenant,
+    updateTenant
 } from '../controllers/tenantControlles'
 
 const router = express.Router();
 
 router.get('/:cognitoId', getTenant);
+router.put('/:cognitoId', updateTenant);
 router.post('/', createTenant);
 
 export default router;
