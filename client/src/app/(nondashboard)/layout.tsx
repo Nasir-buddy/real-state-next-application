063f08ba71c345744f6e72533372d59b8f315a3a
Nasir-Buddy <syedali172000@gmail.com>
2025-03-21 02:15:11 +0530

    set the same code for teh nondashboard

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -6,7 +6,35 @@ import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   const { data: authUser } = useGetAuthUserQuery();
-  console.log("authUser", authUser);
+ const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (userRole === "manager" && pathname.startsWith('/tenants')
+        || (userRole === "tenant" && pathname.startsWith('/managers'))) {
+        router.push(
+          userRole === 'manager' ?
+            '/managers/properties'
+            : '/tenants/favorites'
+        );
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname])
+
+
+   // Show loading state while fetching user data
+   if (authLoading || isLoading) {
+    return <div className="min-h-screen w-full bg-primary-100 flex items-center justify-center">Loading...</div>;
+  }
+
+  // If no user role is found, you might want to handle this case
+  if (!authUser?.userRole) {
+    return <div className="min-h-screen w-full bg-primary-100 flex items-center justify-center">User role not found</div>;
+  }
 
   return (
     <div className='h-full w-full'>
